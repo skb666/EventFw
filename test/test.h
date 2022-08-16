f00552ce20908ff90be4572af8e2888a17171564
dogming <event-os@outlook.com>
2022-08-16 09:52:45 +0800

    TEST 05-1和6测试通过

--- test/test.h
+++ test/test.h
@@ -15,10 +15,10 @@
 #define TEST_EN_02_2                    0
 #define TEST_EN_03                      0
 #define TEST_EN_04                      0
-#define TEST_EN_05_0                    1
+#define TEST_EN_05_0                    0
 #define TEST_EN_05_1                    0
 #define TEST_EN_06                      0
-#define TEST_EN_07                      0
+#define TEST_EN_07                      1
 #define TEST_EN_09                      0
 
 #endif
