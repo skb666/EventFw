26995e1d2570316c0d763d4af867254dce207daf
dogming <event-os@outlook.com>
2022-08-27 21:33:13 +0800

    Coding template updated.

--- documentation/template/template.c
+++ documentation/template/template.c
@@ -1,16 +1,98 @@
+/*
+ * EventOS Template V0.2
+ * Copyright (c) 2022, EventOS Team, <event-os@outlook.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * https://www.event-os.cn
+ * https://github.com/event-os/eventos
+ * https://gitee.com/event-os/eventos
+ * 
+ * Change Logs:
+ * Date           Author        Notes
+ * 2021-11-23     Dog           the first version
+ * 2022-08-27     Dog           V0.2
+ */
+
+/* include ------------------------------------------------------------------ */
 #include "template.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* private define ----------------------------------------------------------- */
+#define EOS_PRIVATE_MACRO                       (128)
+
+/* private typedef ---------------------------------------------------------- */
+/*
+ * Commit.
+ */
+typedef struct eos_private_type
+{
+    eos_u32_t u32;
+    eos_s32_t s32;
+    eos_u16_t u16;
+    eos_s16_t s16;
+} eos_private_type_t;
+
+/* private variables -------------------------------------------------------- */
+/*
+ * Commit.
+ */
+static eos_private_type_t types;
+
+/* private function prototype ----------------------------------------------- */
+static eos_u32_t _func_private(void *buff, eos_u32_t size);
+
+/* public function ---------------------------------------------------------- */
+/*
+ * Commit.
+ */
 eos_s32_t eos_module_init(void)
 {
     return 0;
 }
 
-void eos_module_poll(eos_time_t time)
+/**
+ * @brief   This function description.
+ * @note    If nessary, write some note here.
+ * @param   time is time.
+ */
+void eos_module_poll(eos_type_t *time)
 {
-    if (time.)
+    /* commit in function. */
+    (void)time;
 }
 
+/**
+ * @brief   This function description.
+ * @note    If nessary, write some note here.
+ */
 void eos_module_end(void)
 {
 
 }
+
+/* private function --------------------------------------------------------- */
+/**
+ * @brief   This function description.
+ * @note    If nessary, write some note here.
+ * 
+ * @param   buff is buff.
+ * @param   size is size.
+ * 
+ * @return  Return current cpu interrupt status.
+ */
+static eos_u32_t _func_private(void *buff, eos_u32_t size)
+{
+    /* commit in function. */
+    (void)buff;
+    (void)size;
+
+    return 0;
+}
+
+#ifdef __cplusplus
+}
+#endif
--- documentation/template/template.h
+++ documentation/template/template.h
@@ -1,26 +1,9 @@
 
 /*
- * EventOS Template V0.1
- * Copyright (c) 2021, EventOS Team, <event-os@outlook.com>
+ * EventOS Template V0.2
+ * Copyright (c) 2022, EventOS Team, <event-os@outlook.com>
  *
- * SPDX-License-Identifier: MIT
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the 'Software'), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
- * copies of the Software, and to permit persons to whom the Software is furnished
- * to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
- * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS 
- * OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
- * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
- * IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * SPDX-License-Identifier: Apache-2.0
  *
  * https://www.event-os.cn
  * https://github.com/event-os/eventos
@@ -28,28 +11,43 @@
  * 
  * Change Logs:
  * Date           Author        Notes
- * 2021-11-23     DogMing       the first version
+ * 2021-11-23     Dog           the first version
+ * 2022-08-27     Dog           V0.2
  */
 
-#ifndef EOS_TEMPLATE_H_
-#define EOS_TEMPLATE_H_
+#ifndef __EOS_TEMPLATE_H__
+#define __EOS_TEMPLATE_H__
 
-// include path ----------------------------------------------------------------
+/* include ------------------------------------------------------------------ */
 #include "eos_def.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-// data structure --------------------------------------------------------------
-typedef struct eos_structure {
+/* public define ------------------------------------------------------------ */
+/*
+ * Commit.
+ */
+#define EOS_MACRO                           (128)
+
+/* public typedef ----------------------------------------------------------- */
+/*
+ * Commit.
+ */
+typedef struct eos_type
+{
     eos_u32_t u32;
     eos_s32_t s32;
     eos_u16_t u16;
     eos_s16_t s16;
-} eos_structure_t;
+} eos_type_t;
 
-typedef struct eos_enum {
+/*
+ * Commit.
+ */
+typedef enum eos_enum
+{
     EosEnum_Tip0 = 0,
     EosEnum_Tip1,
     EosEnum_Tip2,
@@ -57,15 +55,18 @@ typedef struct eos_enum {
     EosEnum_Maxd
 } eos_enum_t;
 
-typedef eos_s32_t (* eos_hook_t)(ek_obj_t *me, void *data, eos_u32_t size);
+/*
+ * Commit.
+ */
+typedef eos_s32_t (* eos_hook_t)(eos_type_t *me, void *data, eos_u32_t size);
 
-// API -------------------------------------------------------------------------
+/* public function ---------------------------------------------------------- */
 eos_s32_t eos_module_init(void);
-void eos_module_poll(eos_time_t time);
+void eos_module_poll(eos_type_t *time);
 void eos_module_end(void);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif
+#endif  /* __EOS_TEMPLATE_H__ */
