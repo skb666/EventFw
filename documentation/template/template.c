dfaa6a30a2c33be5dd70f3ab9c90e983e1d5eae7
GouGe <event-os@outlook.com>
2023-11-29 14:46:10 +0800

    Add C source and header files templates.

--- documentation/template/template.c
+++ documentation/template/template.c
@@ -1,16 +1,66 @@
-#include "template.h"
+/*
+ * eLab Project
+ * Copyright (c) 2023, EventOS Team, <event-os@outlook.com>
+ */
 
-eos_s32_t eos_module_init(void)
+/* includes ----------------------------------------------------------------- */
+#include "elab_device.h"
+#include "elab_device_def.h"
+#include "../common/elab_assert.h"
+#include "../common/elab_log.h"
+#include "../os/cmsis_os.h"
+
+ELAB_TAG("EdfDevice");
+
+/* private config ----------------------------------------------------------- */
+
+/* private defines ---------------------------------------------------------- */
+
+/* private function prototypes ---------------------------------------------- */
+static void _add_device(elab_device_t *me);
+static osMutexId_t _edf_mutex(void);
+
+/* private variables -------------------------------------------------------- */
+static uint32_t _edf_device_count = 0;
+static elab_device_t *_edf_table[ELAB_DEV_NUM_MAX];
+static osMutexId_t _mutex_edf = NULL;
+
+/**
+ * The edf global mutex attribute.
+ */
+static const osMutexAttr_t _mutex_attr_edf =
 {
-    return 0;
+    .name = "mutex_edf",
+    .attr_bits = osMutexPrioInherit | osMutexRecursive,
+    .cb_mem = NULL,
+    .cb_size = 0, 
+};
+
+/* public function ---------------------------------------------------------- */
+/**
+ * This function registers a device driver with specified name.
+ * @param dev the pointer of device driver structure
+ * @param name the device driver's name
+ * @return None.
+ */
+void elab_device_register(elab_device_t *me, elab_device_attr_t *attr)
+{
+    assert(me != NULL);
+    assert(attr != NULL);
+    assert(attr->name != NULL);
+    assert_name(elab_device_find(attr->name) == NULL, attr->name);
 }
 
-void eos_module_poll(eos_time_t time)
+/* private functions -------------------------------------------------------- */
+static osMutexId_t _edf_mutex(void)
 {
-    if (time.)
+    if (_mutex_edf == NULL)
+    {
+        _mutex_edf = osMutexNew(&_mutex_attr_edf);
+        assert(_mutex_edf != NULL);
+    }
+
+    return _mutex_edf;
 }
 
-void eos_module_end(void)
-{
-
-}
+/* ----------------------------- end of file -------------------------------- */
--- documentation/template/template.h
+++ documentation/template/template.h
@@ -1,71 +1,138 @@
-
 /*
- * EventOS Template V0.1
- * Copyright (c) 2021, EventOS Team, <event-os@outlook.com>
- *
- * SPDX-License-Identifier: MIT
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the 'Software'), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
- * copies of the Software, and to permit persons to whom the Software is furnished
- * to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
- * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS 
- * OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
- * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
- * IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- *
- * https://www.event-os.cn
- * https://github.com/event-os/eventos-nano
- * https://gitee.com/event-os/eventos-nano
- * 
- * Change Logs:
- * Date           Author        Notes
- * 2021-11-23     DogMing       the first version
+ * eLab Project
+ * Copyright (c) 2023, EventOS Team, <event-os@outlook.com>
  */
 
-#ifndef EOS_TEMPLATE_H_
-#define EOS_TEMPLATE_H_
+#ifndef ELAB_DEVICE_H
+#define ELAB_DEVICE_H
 
-// include path ----------------------------------------------------------------
-#include "eos_def.h"
+/* includes ----------------------------------------------------------------- */
+#include <stdbool.h>
+#include <stdint.h>
+#include <string.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-// data structure --------------------------------------------------------------
-typedef struct eos_structure {
-    eos_u32_t u32;
-    eos_s32_t s32;
-    eos_u16_t u16;
-    eos_s16_t s16;
-} eos_structure_t;
+/* public config ------------------------------------------------------------ */
+#define ELAB_DEV_NUM_MAX                (64)
+#define ELAB_DEV_PALTFORM               ELAB_PALTFORM_RTOS
 
-typedef struct eos_enum {
-    EosEnum_Tip0 = 0,
-    EosEnum_Tip1,
-    EosEnum_Tip2,
+/* public define ------------------------------------------------------------ */
+enum elab_device_type
+{
+    ELAB_DEVICE_NULL = 0,
 
-    EosEnum_Maxd
-} eos_enum_t;
+    ELAB_DEVICE_PIN,
+    ELAB_DEVICE_PWM,
+    ELAB_DEVICE_ADC,
+    ELAB_DEVICE_DAC,
+    ELAB_DEVICE_UART,
+    ELAB_DEVICE_I2C_BUS,
+    ELAB_DEVICE_I2C,
+    ELAB_DEVICE_SPI_BUS,
+    ELAB_DEVICE_SPI,
+    ELAB_DEVICE_CAN,
+    ELAB_DEVICE_WATCHDOG,
+    ELAB_DEVICE_RTC,
+    ELAB_DEVICE_UNKNOWN,
 
-typedef eos_s32_t (* eos_hook_t)(eos_obj_t *me, void *data, eos_u32_t size);
+    ELAB_DEVICE_NORMAL_MAX,
+};
 
-// API -------------------------------------------------------------------------
-eos_s32_t eos_module_init(void);
-void eos_module_poll(eos_time_t time);
-void eos_module_end(void);
+enum elab_device_layer
+{
+    ELAB_DEV_LAYER_NORAML = 0,                  /* On-chip device mode */
+    ELAB_DEV_LAYER_USER,                        /* User defined device mode */
+};
+
+enum elab_device_boot_level
+{
+    ELAB_DEV_BOOT_L0 = 0,                       /* The lowest */
+    ELAB_DEV_BOOT_L1,
+    ELAB_DEV_BOOT_L2,
+    ELAB_DEV_BOOT_L3,
+    ELAB_DEV_BOOT_L4,
+    ELAB_DEV_BOOT_L5,
+    ELAB_DEV_BOOT_L6,
+    ELAB_DEV_BOOT_L7,
+};
+
+/* public typedef ----------------------------------------------------------- */
+typedef struct elab_device_attr
+{
+    const char *name;
+    bool sole;
+    uint8_t type;
+} elab_device_attr_t;
+
+typedef struct elab_device
+{
+    elab_device_attr_t attr;
+
+    uint8_t enable_count;
+#if (ELAB_DEV_PALTFORM == ELAB_PALTFORM_RTOS)
+    uint8_t lock_count;
+    osMutexId_t mutex;
+#endif
+    osThreadId_t thread_test;
+
+    /* common device interface */
+    const struct elab_dev_ops *ops;
+    void *user_data;
+} elab_device_t;
+
+typedef struct elab_dev_ops
+{
+    elab_err_t (* enable)(elab_device_t *me, bool status);
+    int32_t (* read)(elab_device_t *me, uint32_t pos, void *buffer, uint32_t size);
+    int32_t (* write)(elab_device_t *me, uint32_t pos, const void *buffer, uint32_t size);
+#if (ELAB_DEV_PALTFORM == ELAB_PALTFORM_POLL)
+    void (* poll)(elab_device_t *me);
+    elab_err_t (* isr_enable)(elab_device_t *me, bool status);
+#endif
+} elab_dev_ops_t;
+
+#define ELAB_DEVICE_CAST(_dev)      ((elab_device_t *)_dev)
+
+/* public functions --------------------------------------------------------- */
+/**
+ * @brief This function registers a device with its atttibutes.
+ * @param me    Device handle
+ * @param attr  the device driver's atttibute.
+ * @return None.
+ */
+void elab_device_register(elab_device_t *me, elab_device_attr_t *attr);
+
+/**
+ * @brief Open the given device.
+ * @param _dev  Device handle.
+ * @return None.
+ */
+#define elab_device_open(_dev)              __device_enable(ELAB_DEVICE_CAST(_dev), true)
+
+/**
+ * @brief Close the given device.
+ * @param _dev  Device handle.
+ * @return None.
+ */
+#define elab_device_close(_dev)             __device_enable(ELAB_DEVICE_CAST(_dev), false)
+
+/* private function --------------------------------------------------------- */
+void __device_mutex_lock(elab_device_t *me, bool status);
+elab_err_t __device_enable(elab_device_t *me, bool status);
+
+/* private define ----------------------------------------------------------- */
+/* eLab platform */
+#define ELAB_PALTFORM_RTOS                  (0)
+#define ELAB_PALTFORM_POLL                  (1)
+#define ELAB_PALTFORM_BASIC_OS              (2)
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif
+#endif /* ELAB_DEVICE_H */
+
+/* ----------------------------- end of file -------------------------------- */
